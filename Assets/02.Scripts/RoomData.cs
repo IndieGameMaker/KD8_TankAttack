4b2fe554cf4d56faf12dcab5c62a658a0fd8034d
JaeHyun Lee <myleje@gmail.com>
2022-02-21 10:18:51 +0900

    .

---
+++ Assets/02.Scripts/RoomData.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomData : MonoBehaviour
+{
+    public TMP_Text roomText;
+    private RoomInfo roomInfo;
+
+    // 프로퍼티 정의
+    public RoomInfo RoomInfo
+    {
+        //Getter ex) Debug.Log(RoomData.RoomInfo);
+        get
+        {
+            return roomInfo;
+        }
+        //Setter ex) RoomData.RoomInfo = {데이터};
+        set
+        {
+            roomInfo = value;
+            // 룸 정보 표시
+            roomText.text = $"{roomInfo.Name} ({roomInfo.PlayerCount}/{roomInfo.MaxPlayers})";
+        }
+    }
+
+
+}
